3788323d74eaedf08f79aadc922fa2c14ecce61c
Fernando Gomez <shadyfast8.services@gmail.com>
2023-02-13 21:56:47 +0100

    Set a limit for stored data of 24 hours

--- src/store/services/podcastApi.ts
+++ src/store/services/podcastApi.ts
@@ -1,9 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { Podcast, PodcastApiResponse } from '@/types'
 
+const SECONDS_IN_A_DAY = 60 * 60 * 24
+
 export const podcastsApi = createApi({
   reducerPath: 'podcastsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://cors-anywhere.herokuapp.com/https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json' }),
+  keepUnusedDataFor: SECONDS_IN_A_DAY,
   endpoints: (builder) => ({
     getAllPodcasts: builder.query<Podcast[], void>({
       query: () => ``,
