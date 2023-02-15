6a81f851f183deebcf29e20d0b15e521a339fac2
Fernando Gomez <shadyfast8.services@gmail.com>
2023-02-15 18:18:22 +0100

    Fix command.ts export and abstract Link to Podcast component

--- src/components/Podcast.tsx
+++ src/components/Podcast.tsx
@@ -10,16 +10,16 @@ export type PodcastProps = {
 const Podcast = ({ podcast }: PodcastProps) => {
   return (
     <li>
-        <Image 
-            alt={podcast.title.label}
-            src={podcast['im:image'][2].label} 
-            height={100} 
-            width={100}
-        />
-        <Link href={`/podcast/${podcast.id.attributes['im:id']}`}>
-          {podcast['im:name'].label.toUpperCase()}
+      <Link href={`/podcast/${podcast.id.attributes['im:id']}`}>
+          <Image 
+              alt={podcast.title.label}
+              src={podcast['im:image'][2].label} 
+              height={100} 
+              width={100}
+          />
+          <p>{podcast['im:name'].label.toUpperCase()}</p>
+          <p>Author: {podcast['im:artist'].label}</p>
         </Link>
-        <p>Author: {podcast['im:artist'].label}</p>
     </li>
   )
 }
