934c8237b1c48f0477f56b15bb1254c900310877
Fernando Gomez <shadyfast8.services@gmail.com>
2023-02-17 19:02:48 +0100

    Update e2e tests

--- cypress/e2e/Podcast.cy.ts
+++ cypress/e2e/Podcast.cy.ts
@@ -1,22 +1,18 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/podcast/1460157002')
+    cy.get('[data-testid="loading"]').should('exist')
 
     cy.get('h1').contains('Podcaster')
-    cy.get('p').contains('Loading ...')
-
     cy.get('[data-testid="loading"]').should('exist')
-    cy.intercept('https://itunes.apple.com/**').as('itunesCall')
 
-    cy.get('[data-testid="loading"]').should('exist')
+    cy.intercept('GET', 'https://itunes.apple.com/**', { statusCode: 200 }).as('itunesCall')
 
     cy.wait('@itunesCall')
 
-    cy.get('p').contains('Episodes: 51')
+    cy.get('span').contains('Episodes: 50')
 
     cy.get('[data-testid="episodes-list"]').children().should('exist')
-
-
   })
 })
 
