f3a8a15bdf8b54cbe3c0f147ad6d45ad7db16049
Fernando Gomez <shadyfast8.services@gmail.com>
2023-02-17 19:51:19 +0100

    Test EpisodeTrack component

---
+++ __tests__/components/EpisodeTrack.spec.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import EpisodeTrack from '../../src/components/EpisodeTrack';
+import { IEpisode } from '../../src/types'
+
+describe('EpisodeTrack', () => {
+
+    const episode = {
+        episodeUrl: '/URL',
+        trackName: 'Track name',
+        collectionName: 'Collection name',
+        artistName: 'Artist name',
+        description: 'Description'
+    } as Partial<IEpisode>
+
+    it('Should render with props', () => {
+       const { container } = render(<EpisodeTrack episode={episode}/>)
+
+        const name = screen.getByText('Artist name - Track name')
+        const collectionName = screen.getByText('Collection name');
+        const description = screen.getByText('Description')
+        const link = container.getElementsByTagName('source')[0]
+
+        expect(name).toBeInTheDocument()
+        expect(collectionName).toBeInTheDocument()
+        expect(description).toBeInTheDocument()
+        expect(link).toHaveAttribute('src', '/URL')
+    })
+})
